71946a7f039c6b1f99f25bd12236a719bcbca69c
ANDREWIDEAPAD\03dre <pattona@southern.edu>
2025-01-10 19:37:21 -0500

    Updated to align with updates to cursestools

--- widget_tests.py
+++ widget_tests.py
@@ -1,6 +1,8 @@
 import curses
 from curses import window
-from cursestools import Panel, Terminal, FreeWindow, TextBox, Dir, Align
+from cursestools import consts as c
+from cursestools import Panel, Terminal, Page, TextBox,\
+    Dir, Align
 from time import sleep
 from random import randrange
 
@@ -9,11 +11,13 @@ def main(stdscr: window):
     stdscr.box()
     stdscr.noutrefresh()
     curses.doupdate()
+    # print(repr(stdscr.getkey()))
+    # raise SystemExit
     
-    textbox_test(stdscr)
-    panel_test(stdscr)
-    terminal_test(stdscr)
-    console_test(stdscr)
+    # textbox_test(stdscr)
+    # panel_test(stdscr)
+    # terminal_test(stdscr)
+    page_test(stdscr)
 
 def panel_test(stdscr: window):
     panel1 = Panel(5, 30, 5, 5, outline=True)
@@ -38,7 +42,7 @@ def panel_test(stdscr: window):
 
 def terminal_test(stdscr: window):
 
-    terminal = Terminal(10, 100, 10, 10)
+    terminal = Terminal(3, 50, 2, 10, '> ')
     for letter in "Bananas in Paris ":
         sleep(0.1)
         terminal.proc_key(letter)
@@ -49,73 +53,48 @@ def terminal_test(stdscr: window):
     stdscr.getch()
     for letter in "BOOOO!":
         terminal.proc_key(letter)
+    terminal.proc_key(c.ENTER)
+    terminal_2 = Terminal(3, 30, 5, 70, "UNPROCESSED KEYS: ")
+    for _ in range(20):
+        terminal_2.proc_key(terminal.proc_key(terminal.getkey()))
     stdscr.getch()
 
 def textbox_test(stdscr: window):
 
     text = "This is a test to see the effectiveness of the textbox widget. How well is it working? You tell me. Or don't tell me. I can do my own testing and determining..."
-
-    textbox = TextBox(4, 60, 2, 30)
+    textbox = TextBox(5, 60)
     textbox.set_text(text)
 
-    textbox.set_align(Align.RIGHT)
-    textbox.read("CHAR")
-
-    stdscr.getch()
-    textbox.set_align(Align.LEFT)
-    textbox.read("CHAR", 0.005)
-
-    stdscr.getch()
-    textbox.set_align(Align.CENTER)
-    textbox.read("WORD", 0.002)
-
+    textbox.print_textbox(stdscr, 7, 3)
     stdscr.getch()
 
-def console_test(stdscr: window):
+    textbox.set_alignment(Align.RIGHT)
+    textbox.read_textbox(stdscr, 7, 64, "CHAR", 0.01)
+    stdscr.getch()
+
+    textbox.set_alignment(Align.CENTER, v_centered=True)
+    textbox.read_textbox(stdscr, 12, (stdscr.getmaxyx()[1] - 60) // 2, "WORD", 0.1)
+    stdscr.getch()
+
+def page_test(stdscr: window):
     ### FILLING CONTENT
-    mycons = FreeWindow(stdscr)
-    max_y, max_x = mycons.getmaxyx()
-    for y in range(max_y - 1):
-        for x in range(max_x - 1):
+    height, width = stdscr.getmaxyx()
+    page = Page(stdscr, multiplier=2)
+    for y in range(height * 2):
+        for x in range(width * 2 - 1):
             num = randrange(32, 126)
             char = chr(num)
-            mycons.addch(y, x, char)
-    mycons.refresh()
-    stdscr.getch()
+            page.addch(y, x, char)
+    try:
+        page.addch(height * 2, width * 2 - 1, char)
+    except curses.error:
+        pass
+    page.refresh()
 
-    ### BOUNDARY BEHAVIOR
-    for _ in range(max_y - 1):
-        sleep(0.005)
-        mycons.shift(Dir.UP)
-        mycons.refresh()
-    stdscr.getch()
-    mycons.shift(Dir.DOWN)
-    mycons.refresh()
-    stdscr.getch()
-
-    mycons.reset_offset()
-    mycons.refresh()
-    stdscr.getch()
-
-    ### STANDARD SCROLLING
-    for _ in range(2):
-        for _ in range(2):
-            mycons.shift(Dir.UP)
-            mycons.refresh()
-            stdscr.getch()
-        for _ in range(2):
-            mycons.shift(Dir.DOWN)
-            mycons.refresh()
-            stdscr.getch()
-    for _ in range(2):
-        for _ in range(2):
-            mycons.shift(Dir.LEFT)
-            mycons.refresh()
-            stdscr.getch()
-        for _ in range(2):
-            mycons.shift(Dir.RIGHT)
-            mycons.refresh()
-            stdscr.getch()
+    binds = {'a': Dir.RIGHT, 's': Dir.UP, 'w': Dir.DOWN, 'd': Dir.LEFT}
+    for _ in range(20):
+        page.shift(binds.get(page.getkey(), Dir.LEFT))
+        page.refresh()
 
 if __name__ == "__main__":
     curses.wrapper(main)
